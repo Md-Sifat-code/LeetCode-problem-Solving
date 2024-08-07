932db84792da0290e41894c30fd6f2396392567c
Md-Sifat_code <codewithsifat6@gmail.com>
2024-08-07 12:32:36 +0600

    Adding the new solved problem of leet code. Remove elements

---
+++ Remove_elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n =nums.length;
+        
+        int index =0;
+        for(int i =0;i<n;i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
