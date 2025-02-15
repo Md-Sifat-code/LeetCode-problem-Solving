beda95b2d74ab650699d482e2b867cf24b45844a
Md-Sifat-code <codewithsifat4@gmail.com>
2025-02-15 20:33:00 +0600

    2/15/2025

---
+++ Missing Number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        n = len(nums)
+        expected_sum = (n * (n + 1)) // 2
+        
+        actual_sum = sum(nums)
+        
+        return expected_sum - actual_sum
